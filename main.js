7f66313d9f2fa0b853a5e228d76c666c9fadaefc
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-02-01 15:04:01 -0500

    working calculator

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 // Calculator parts stored in a variable
-let calSquares = document.querySelectorAll(".button");
+let calSquares = document.querySelectorAll(".notEquals");
 let displayScreen = document.querySelector('#display');
+let equalSign = document.querySelector('#equal');
+let clearButton = document.querySelector('#clear')
+
 
 //Displays the numbers in the calculator screen
-const fullDisplay = [];
-for (let part of calSquares) {
-    part.addEventListener('click', function (event) {
+let fullDisplay = [];
+for (let square of calSquares) {
+    square.addEventListener('click', function (event) {
         let userInput = event.target.innerText;
         fullDisplay.push(userInput);
         // console.log(fullDisplay)
-        displayScreen.innerText = fullDisplay;
+        displayScreen.innerText = fullDisplay.join("");
     }
     )
 }
+
+equalSign.addEventListener('click', function (event) {
+    //clicking equal button evaluate the expression -- display solution
+    let answer = eval(displayScreen.innerText)
+    console.log(answer)
+    displayScreen.innerText = answer;
+}
+)
+
+clearButton.addEventListener('click', function (event) {
+    displayScreen.innerText = "";
+}
+)
