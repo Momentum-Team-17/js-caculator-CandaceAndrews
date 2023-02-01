20df371e9afbf02e30d7d7ef51e4b96d1ff286ce
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-01-31 19:43:18 -0500

    Got numbers to display on screen when clicked

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+// Calculator parts stored in a variable
 let calSquares = document.querySelectorAll(".button");
+let displayScreen = document.querySelector('#display');
 
+
+
+// Display clicked number in console log and screen
 for (let part of calSquares) {
     part.addEventListener("click", function (event) {
-        console.log(event.target.innerText)
+        // console.log(event.target.innerText)
+        let output = event.target.innerText;
+        displayScreen.innerText = output;
     }
     )
 }
+
